44e8db94cd15e6ea6c17c0980a13519c7e1f7a2d
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2022-11-03 13:38:45 +0530

    Update fun.js

--- 28 aug/fun.js
+++ 28 aug/fun.js
@@ -1,36 +1,36 @@
 console.log("Hii")
 
-//ou can add timwe when your fuction is run
-// setTimeout(() => {
-//     console.log("Hello")
-// }, 2000);
+// ou can add timwe when your fuction is run
+setTimeout(() => {
+    console.log("Hello")
+}, 2000);
 
-// setTimeout(() => {
-//     console.log("how are you")
-// },3000);
+setTimeout(() => {
+    console.log("how are you")
+},3000);
 
 // I am creating function in function
 
 
-// function ad(){
-//     function b(){
-//         console.log('hii')
-//     }
-//     b();
-//     console.log('hiii')
-// }
-// ad()
+function ad(){
+    function b(){
+        console.log('hii')
+    }
+    b();
+    console.log('hiii')
+}
+ad()
 
 
-// function add(a,b,handle){
-//     let result = a+b;
-//     handle(result)
-// }
+function add(a,b,handle){
+    let result = a+b;
+    handle(result)
+}
 
-// function print(result){
-//     console.log(`printing your result ${result}`)
-// }
-// add(4,5,print)
+function print(result){
+    console.log(`printing your result ${result}`)
+}
+add(4,5,print)
 
 
 function add(){
