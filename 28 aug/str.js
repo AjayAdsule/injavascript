9cec2899d07ecf8c9b44de4761003d67e3727122
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2022-11-06 21:37:25 +0530

    Create str.js

---
+++ 28 aug/str.js
@@ -0,0 +1,12 @@
+let str="Ajay is programmer"
+//console.log(str.length) 18
+var a=str.toUpperCase()
+console.log(a)
+var a=str.toLocaleLowerCase()
+console.log(a)
+var a=str.includes("is") //true
+console.log(a)
+var a=str.endsWith("is") //false
+console.log(a)
+var a =str.endsWith("er") //true
+console.log(a)
