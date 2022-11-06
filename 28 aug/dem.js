02fa783bf151c661dbdfa94b0422c71792078021
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2022-11-06 20:56:36 +0530

    Update dem.js

--- 28 aug/dem.js
+++ 28 aug/dem.js
@@ -1,29 +1,29 @@
-// class student{
-//     constructor(name,age,yop){
-//         this.name=name;
-//         this.age=age;
-//         this.yop=yop;
-//         this.surname="Adsule";
-//     }
-//     greet(){
-//         console.log(`your name is ${this.name}`)
-//         console.log(`your age is${this.age}`)
-//         console.log(`your passing year is ${this.yop}`)
-//         console.log(`your surname is ${this.surname}`)
-//     }
-// }
-// let person= new student ("ajay",22,1999)
-// person.greet()
+class student{
+    constructor(name,age,yop){
+        this.name=name;
+        this.age=age;
+        this.yop=yop;
+        this.surname="Adsule";
+    }
+    greet(){
+        console.log(`your name is ${this.name}`)
+        console.log(`your age is${this.age}`)
+        console.log(`your passing year is ${this.yop}`)
+        console.log(`your surname is ${this.surname}`)
+    }
+}
+let person= new student ("ajay",22,1999)
+person.greet()
 
-// let add= (a,b)=>{
-//     console.log(a+b)
-// }
-// add(5,4)
+let add= (a,b)=>{
+    console.log(a+b)
+}
+add(5,4)
 
 
-// for(let i=0;i<10;i++){
-//     console.log(i)
-// }
+for(let i=0;i<10;i++){
+    console.log(i)
+}
 
 function dem(){
     let a=10;
@@ -33,5 +33,5 @@ function dem(){
     }
 }
 let result=dem()
-// result(7)
+result(7)
 // dem()
