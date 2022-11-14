ee4be5a62d31e9bc22aa04458308c1a597d12655
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2022-11-14 13:37:34 +0530

    regtax

--- 3sept/index.js
+++ 3sept/index.js
@@ -1,31 +1,32 @@
-// var ma=Math.ceil(3.9) //it will give higher value
-// console.log(ma)
-// var ma=Math.random()// It will give you random value
-// console.log(ma)
-// var ma=Math.floor(4.9) //It will give lower value
-// console.log(ma)
-// var ma=Math.round(9.5)// it will give you round up value
-// console.log(ma)
-// var ma=Math.round(9.4)// it will give you round up value
-// console.log(ma)
-// var ma= Math.trunc(6.906521)// After the decimal value rthey will igonre the value
-// console.log(ma)
+var ma=Math.ceil(3.9) //it will give higher value
+console.log(ma)
+var ma=Math.random()// It will give you random value
+console.log(ma)
+var ma=Math.floor(4.9) //It will give lower value
+console.log(ma)
+var ma=Math.round(9.5)// it will give you round up value
+console.log(ma)
+var ma=Math.round(9.4)// it will give you round up value
+console.log(ma)
+var ma= Math.trunc(6.906521)// After the decimal value rthey will igonre the value
+console.log(ma)
 
 //Negative maths
-// var m=Math.ceil(-3.9)// In negative ceil will give you lower value
-// console.log(m)
-// var m=Math.floor(-4.1) //In negative it will give you higher value
-// console.log(m)
-// var m=Math.round(-4.4)//In negative it will give round value
-// console.log(m)
+var m=Math.ceil(-3.9)// In negative ceil will give you lower value
+console.log(m)
+var m=Math.floor(-4.1) //In negative it will give you higher value
+console.log(m)
+var m=Math.round(-4.4)//In negative it will give round value
+console.log(m)
 
-// var c=Math.min(10,45,74,6,4,852,54,54,5223) //It will give you minimum value
-// console.log(c)
-// var c=Math.max(45,454,2525,2325,4746,25,)// It will give you maximum number 
-// console.log(c)
+var c=Math.min(10,45,74,6,4,852,54,54,5223) //It will give you minimum value
+console.log(c)
+var c=Math.max(45,454,2525,2325,4746,25,)// It will give you maximum number 
+console.log(c)
 var c=Math.sqrt(8)//it will give you square root number
 console.log(c)
 var c=Math.abs(-5.74)// it will give you same value
 console.log(c)
 var c=Math.PI+1;//It will give you pie vqalue
 console.log(c)
+
---
+++ 3sept/regax.js
@@ -0,0 +1,8 @@
+let a= "5blockchain"
+let regax=/[1-5]blockchain/;
+if(regax.test(a)){
+    console.log("true")
+}
+else{
+    console.log('false')
+}
