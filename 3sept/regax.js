487fbf7353e81d1bf6af2febd9f54d39120a2bf1
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2022-11-14 14:14:47 +0530

    regax

--- 3sept/regax.js
+++ 3sept/regax.js
@@ -1,8 +1,18 @@
-let a= "5blockchain"
-let regax=/[1-5]blockchain/;
-if(regax.test(a)){
-    console.log("true")
+// let a= "5Blockchain"
+// let regax=/[^1-5]blockchain/i;//
+// if(regax.test(a)){
+//     console.log("true")
+// }
+// else{
+//     console.log('false')
+// }
+
+// validating mobile number through regax\
+let b=7977223876
+let re=/[6-9][0-9]{9}/;//(in curly bracket 9 is write for the number is count 9 times )
+if(re.test(b)){
+    console.log("valid number ")
 }
 else{
-    console.log('false')
+    console.log("invalid number")
 }
