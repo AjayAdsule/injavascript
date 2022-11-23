51b69b6ffe6f2c71626f8149a7e424a2d613d617
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2022-11-23 10:37:52 +0530

    Update promis.js

--- 3sept/promis.js
+++ 3sept/promis.js
@@ -1,10 +1,16 @@
+//This is the executor function aka the person
 const promise=new Promise((resolve, reject) => {
     resolve('i am bring the water')
+    reject('i am not get water')
 })
 
+//Consumer function one who will receive promise
 const cheif=()=>{
     promise.then(function(result){
         console.log(`starting thre cooking ${result}`)
+    })
+    promise.catch(function (error){
+        console.log(`ohh you did ${error.message}`)
     })
 }
 cheif()
